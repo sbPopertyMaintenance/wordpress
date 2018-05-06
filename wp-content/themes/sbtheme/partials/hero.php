d224a89eae62b05255f4624eae5226733d221b8d
sbPropertyMaintenance <sbpropertymaintenanceNS@gmail.com>
2018-05-06 14:11:55 -0300

    Updated get_theme_mod()

--- wp-content/themes/sbtheme/partials/hero.php
+++ wp-content/themes/sbtheme/partials/hero.php
@@ -1,7 +1,7 @@
 <section class="row hero" style="background-image: url(<?php header_image(); ?>)">
   <div class="container">
-      <h1><?php echo get_theme_mod( 'header_slogan' ); ?></h1>
-      <p><?php echo get_theme_mod( 'header_subtext' ); ?></p>
+      <h1><?php echo get_theme_mod( 'header_slogan', 'THE COMPLETE PROPERTY MAINTENANCE SOLUTION' ); ?></h1>
+      <p><?php echo get_theme_mod( 'header_subtext', 'We can bring your property to life!' ); ?></p>
       <div class="hero-buttons">
         <a href="#" class="btn btn-primary">Get a Quote</a>
         <a href="#" class="btn btn-outline">Learn More</a>
